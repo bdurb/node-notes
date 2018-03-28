1f5612efadeaa99956d7f2dcb2fffae76a8c56fe
bdurb <brian@rpmgreenville.com>
2018-03-28 15:51:55 -0400

    created notes folder, imported into app

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ console.log('starting app.')
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
 const user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
