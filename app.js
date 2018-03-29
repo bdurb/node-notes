e59faae31f315ce9b5d1c814af74aac659840809
bdurb <brian@rpmgreenville.com>
2018-03-28 22:01:50 -0400

    setting up basic note logic

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ console.log('starting app.')
 const fs = require('fs');
 const _ = require('lodash');
 
+const commant = process.argv[2];
 const notes = require('./notes');
+
+if (commant === 'add') {
+  console.log('Adding new Note')
+} else if (commant === 'list') {
+  console.log('listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('command not recgonized');
+}
