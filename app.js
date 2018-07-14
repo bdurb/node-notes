1bfdaddae3a27d14e92359abc6ecf79ff15747d4
bdurb <brian@rpmgreenville.com>
2018-07-14 16:12:03 -0700

    added commands in yargs

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: {
+      describe: 'Body of note',
+      demand: true,
+      alias: 'b'
+    }
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 const command = argv._[0];
 
 if (command === 'add') {
